48cc18755c854ebd23c778928f524c9d1b88d2a8
yyijian <yyijian601@gmail.com>
2016-05-18 14:21:31 +0800

    fix the Search's animate Bug

--- js/mi.js
+++ js/mi.js
@@ -4,12 +4,15 @@ $(document).click(function() {
     $(".search-txt").css("border", "1px solid #e0e0e0");
     $(".search-btn").css("border", "1px solid #e0e0e0");
     $(".search-btn").css("border-left", "none");
-    $(".hot-word1,.hot-word2").animate({
+    /*$(".hot-word1,.hot-word2").animate({
         opacity: 1
     }, 300,function() {
-		/* stuff to do after animation is complete */
 		$(".hot-word1,.hot-word2").show();
-	});
+	});*/
+    //$(".hot-word1,.hot-word2").show(300,console.log('show ok!'));
+    $(".hot-word1,.hot-word2").fadeIn('400', function() {
+        console.log('show ok!')
+    });
 });
 
 $(".search-box").click(function() {
@@ -18,25 +21,33 @@ $(".search-box").click(function() {
     $(".search-txt").css("border-bottom", "none");
     $(".search-btn").css("border", "1px solid #ff6700");
     $(".search-btn").css("border-left", "none");
-    $(".hot-word1,.hot-word2").animate({
+    $(".hot-word1,.hot-word2").fadeOut('400', function() {
+        console.log('hide ok!')
+    });
+    /*$(".hot-word1,.hot-word2").animate({
         opacity: 0
     }, 300, function() {
-		/* stuff to do after animation is complete */
 		$(".hot-word1,.hot-word2").hide();
-	});
+	});*/
     return false;
 });
 
 $(".search-box").mouseover(function() {
-	/*$(".search-txt,.search-btn").animate({
-		borderColor: #666666
-	},300);*/
-	$(".search-txt").css("border", "1px solid #666666");
-	$(".search-txt").css("border-right","none");
-	$(".search-btn").css("border", "1px solid #666666");
+    /*$(".search-txt,.search-btn").animate({
+    	borderColor: #666666
+    },300);*/
+    var isFocus=$(".search-txt").is(":focus");  
+    if (!isFocus) {
+        $(".search-txt").css("border", "1px solid #666666");
+        $(".search-txt").css("border-right", "none");
+        $(".search-btn").css("border", "1px solid #666666");
+    }
 });
 $(".search-box").mouseout(function(event) {
-	$(".search-txt").css("border", "1px solid #e0e0e0");
-	$(".search-txt").css("border-right","none");
-	$(".search-btn").css("border", "1px solid #e0e0e0");
+	var isFocus=$(".search-txt").is(":focus");  
+    if (!isFocus) {
+        $(".search-txt").css("border", "1px solid #e0e0e0");
+        $(".search-txt").css("border-right", "none");
+        $(".search-btn").css("border", "1px solid #e0e0e0");
+    }
 });
